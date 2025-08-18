baafc12dd85e1dbe20143f8b34a008052f3d73bc
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-08-18 09:31:55 +0800

    feat: Made Navbar global

--- mymodule.js
+++ mymodule.js
@@ -1,27 +1,3 @@
-const fullnames = ["Juan Dela Cruz", "Pedro Penduko", "Tigang Akyat"]
+const header = "WHERE DUTY MEETS DEDICATION.";
 
-const students = [
-    {
-        name: fullnames[0], 
-        age: 20, 
-        course: 'BSIT'
-    },
-    {
-        name: fullnames[1], 
-        age: 21, 
-        course: 'BSIT'
-    },
-    {
-        name: fullnames[2], 
-        age: 22, 
-        course: 'BSIT'
-    },
-]
-
-// console.log(students) 
-
-const hello = function(name = "Carlo") {
-    return (`Hello ${name}`)
-}
-
-module.exports = {students, fullnames, hello}
+module.exports = { header };
